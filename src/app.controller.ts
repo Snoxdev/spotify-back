8f57e7672ad901b4baa6a7aeb5a69b2894efb42b
silvano <snoxdev@github.com>
2024-07-11 21:45:03 -0400

    segunda

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Artista } from "./artistas";
+
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+   // this.artista.push(new Artista (1, "System of a down", "Rock", "USA", "Rock", 560));
+  }
+
+  artistas: Artista[] = [];
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get ("artistas")
+  obtenerArtista(): Artista[] {
+    //this.artista.push(new Artista (1, "System of a down", "Rock", "USA", "Rock", 560));
+    return this.artistas;
+  }
+
+  @Get ("artistas/:id")
+  obtenerID( @Param("id") id: number): Artista[] {
+    //codigo de busqueda
+    return this.artistas[0];
+  }
+
+  @Post ("artistas")
+  nuevoArtista( @Body() artistas:Artista): Artista {
+    this.artistas.push(artistas);
+    return null;
+  } 
+
+
 }
---
+++ src/artistas.ts
@@ -0,0 +1,12 @@
+export class Artista {
+
+constructor(
+    public id: number,
+    public nombre: string,
+    public tipo: string,
+    public paisOrigen: string,
+    public generoMusical: string,
+    public oyentesMensuales: number
+){}
+
+}
